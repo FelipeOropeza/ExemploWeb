aea4aab50bcc27da1175f664283f7702c7c3f9af
Lau e Lipe <lauan.amorim@etec.sp.gov.br>
2022-11-09 13:40:07 -0300

    Add project files.

---
+++ ExemploWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using ExemploWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Usuario()
+        {
+            var usuario = new Usuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Usuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", usuario);
+            }
+            return View(usuario);
+        }
+    }
+}
---
+++ ExemploWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExemploWeb.MvcApplication" Language="C#" %>
